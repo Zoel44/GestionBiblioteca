347d769a8f804b513ef0b39ef835ea988cf4eb10
z0g_4 <zoelgauler@gmail.com>
2026-06-20 20:13:00 -0300

    corrección de commit fallido

--- 0_Domain/Entities/Loan.cs
+++ 0_Domain/Entities/Loan.cs
@@ -2,7 +2,6 @@ namespace GestionBiblioteca.Domain.Entities
 {
     public class Loan
     {
-<<<<<<< HEAD
         public string Id { get; private set; }
         public DateTime RetirementDate { get; private set; }
         public DateTime ReturnDate { get; private set; }
@@ -25,14 +24,6 @@ namespace GestionBiblioteca.Domain.Entities
             
         }
         
-=======
-        public DateTime RetirementDate { get; private set; }
-        public DateTime ReturnDate { get; private set; }
-        public Book LoandedBook { get; private set; }
-        public Associated AnAssociated { get; private set; }
-
-        
->>>>>>> a50fcbf1f4418dba8e21a757e3c6c1176787b952
 
 
     }
--- 0_Domain/Entities/Professor.cs
+++ 0_Domain/Entities/Professor.cs
@@ -6,7 +6,6 @@ namespace GestionBiblioteca.Domain.Entities
 
         public Professor(int p_dni, string p_name, List<Loan> p_aLoan, string p_subject)
             : base(p_dni, p_name, p_aLoan, 5)
-<<<<<<< HEAD
         {//constructor con historial de prestamos
             Subject = p_subject;
         }
@@ -21,20 +20,5 @@ namespace GestionBiblioteca.Domain.Entities
         }
 
 
-=======
-        {
-            Subject = p_subject;
-        }
-        public Professor(int p_dni, string p_name, string p_subject)
-            : base(p_dni, p_name, 5)
-        {
-            Subject = p_subject;
-        }
-        public override string TypeOfAssociated()
-        {
-            return "Professor";
-        }
-    
->>>>>>> a50fcbf1f4418dba8e21a757e3c6c1176787b952
     }
 }
